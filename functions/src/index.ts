ab16048f1f5a8070f74603bb4c61240121b02398
franco <ratiuxjara43@gmail.com>
2020-08-05 08:41:59 -0600

    BackEnd Settings

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as express from "express";
+const app  = express();
 
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+exports.app = functions.https.onRequest(app);
